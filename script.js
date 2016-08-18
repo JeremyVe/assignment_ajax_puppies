4fd6e652a9a4c15fb30cced4299a185249fd16a9
JeremyVe <jepschx@gmail.com>
2016-08-18 11:41:25 +0800

    puppy puppy done

--- script.js
+++ script.js
@@ -1,40 +1,98 @@
 $(document).ready(function() {
 
+	var $list = $('#puppy-list');
+
+	var errors = [];
+
+	var breedList;
+
+	var $notification = $('.notification');
+
+	$notification.hide();
+
+	$(document).ajaxSuccess(function() {
+		$notification.html("Waiting...");
+		$notification.addClass("waiting");
+		$notification.show();
+
+		setTimeout(function() {
+			if ($notification.html() === "Waiting...") {
+				$notification.html("Still Waiting...")
+			}
+		}, 1000);
+	})
+
+
+	$(document).ajaxComplete(function() {
+		if (errors.length > 0) {
+			$notification.show();
+			$notification.html(errors[0]);
+			$notification.removeClass("waiting").addClass("error");
+			errors = [];
+		} else {
+			$notification.html("success");
+			$notification.removeClass("waiting").addClass("success");
+		}
+		setTimeout(function() {
+			$notification.fadeOut();
+		}, 2000);
+	})
+
+
+
 	$("#update-list").on('click', function(event) {
 		event.preventDefault();
 		$.ajax({
 			url: "https://ajax-puppies.herokuapp.com/puppies.json",
-			success: function(data, status, obj) {
-
-				updatePuppyList(data);				
-				
+			success: function(data) {
+				updatePuppyList(data);					
+			},
+			error: function() {
+				errors.push("Could not update the list");
 			}
 		})
-
-		var updatePuppyList = function(data) {
-			var $list = $('#puppy-list');
-			$list.html("");
-
-			for (var i = 0; i < data.length; i++) {
-
-				var puppy = data[i];
-				console.log(puppy);
-
-				var li = $('<li>')
-				li.html( puppy.name + ", (" + puppy.breed.name + "), " + puppy.created_at );
-				$('#puppy-list').append(li);
-			}
-		}
 	})
+
+	var createPuppyItem = function(puppy) {
+		var li = $('<li>')
+			.attr("id", puppy.id)
+			.html( puppy.name + ", (" + puppy.breed.name + "), " + puppy.created_at );
+
+		return li;
+			
+	}
+
+
+	var updatePuppyList = function(data) {
+		$list.html("");
+
+		for (var i = 0; i < data.length; i++) {
+
+			var puppy = data[i];
+
+
+			var li = createPuppyItem(puppy);
+
+			var adoptLink = createAdoptLink(puppy.id);
+
+
+			$('#puppy-list').append(li);
+			$('#puppy-list').append(adoptLink);
+		}
+	}
+
 
 	var createBreedList = function() {
 		$.ajax({
+			global: false,
 			url: "https://ajax-puppies.herokuapp.com/breeds.json",
-			success: function(data, status, obj) {
+			success: function(data) {
 				updateSelectList(data);
+				breedList = data;
 			}
 		})
 	}
+
 
 	var updateSelectList = function(data) {
 		var $selectList = $('select');
@@ -51,7 +109,6 @@ $(document).ready(function() {
 	createBreedList();
 
 
-
 	$('input[type="button"]').on('click', function(event) {
 
 		var formData = {
@@ -59,22 +116,68 @@ $(document).ready(function() {
 			breed_id: $('select').val()
 		}
 
-		formData = JSON.stringify(formData);
+		if (formData.name.length < 1 ) {
+			errors.push("This puppy needs a name !");
+		} else if (formData.breed_id === undefined) {
+			errors.push("This puppy needs a breed !")
 
-		$.ajax({
-			url: "https://ajax-puppies.herokuapp.com/puppies.json",
-			type: "POST",
-			contentType: 'application/json',
-			data: formData,
-			dataType: 'json',
-			success: function(data, status, obj) {
-				console.log(data);
-			},
-			error: function(data) {
-				console.log(data);
-			}	
-		})
+		} else {
+
+			formData = JSON.stringify(formData);
+
+			$.ajax({
+				url: "https://ajax-puppies.herokuapp.com/puppies.json",
+				type: "POST",
+				contentType: 'application/json',
+				data: formData,
+				dataType: 'json',
+				success: function(data) {
+
+					var breed = breedList[data.breed_id - 1].name;
+					data.breed = {};
+					data.breed.name = breed;
+					var li = createPuppyItem(data);
+
+					$list.prepend(li)
+				},
+				error: function(data) {
+					alert("New puppy not create");
+				}	
+			})
+		}
 		
 	});
+
+	var createAdoptLink = function(id) {
+		var link = $('<a>')
+				   .addClass("adopt")
+				   .attr('id', id)
+				   .attr("href", "#")
+				   .html('Adopt Me!');
+		return link;
+	}
+
+	$('ul').on('click', '.adopt', function(event) {
+		var $el = $(event.target);
+		event.preventDefault();
+
+		var id = $el.attr('id');
+		$.ajax({
+			url: "https://ajax-puppies.herokuapp.com/puppies/" + id + ".json",
+			type: "DELETE",
+			success: function(data) {
+				alert("success", data);
+				removePuppyList(id);
+			},
+			error: function(data) {
+				alert("Puppy not adopted");
+			}	
+
+		})
+	})
+
+	var removePuppyList = function(id) {
+		$('ul').find("[id=" + id + "]").remove();
+	}
 
 })
